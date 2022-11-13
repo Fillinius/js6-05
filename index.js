1fa149eea54fe052d315be4ccbfed69d86052a9b
Fillinius <113914362+Fillinius@users.noreply.github.com>
2022-11-13 09:10:36 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let sum = 0
+
+// for (let i = 0; i <= 100; i += 1) {
+//   console.log(i)
+//   sum += i
+// }
+
+for (let i = 100; i >= 0; i -= 1) {
+  console.log(i)
+  sum += i
+}
+
+// console.log(sum)
+alert(sum)
